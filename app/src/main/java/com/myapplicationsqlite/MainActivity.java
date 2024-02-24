fd335c0006c8e15c7eec7cfe4132c6fc9a465e01
fullstack-khamlichi <114249953+fullstack-khamlichi@users.noreply.github.com>
2024-02-24 13:19:40 +0100

    3nd Commit : Authentification Back

--- app/src/main/java/com/myapplicationsqlite/MainActivity.java
+++ app/src/main/java/com/myapplicationsqlite/MainActivity.java
@@ -2,13 +2,35 @@ package com.myapplicationsqlite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etLogin, etPassword;
+    Button bLogin, bSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etLogin = findViewById(R.id.etLogin);
+        etPassword = findViewById(R.id.etPassword);
+        bLogin = findViewById(R.id.bLogin);
+        bSignUp = findViewById(R.id.bSignUp);
+
+        DatabaseHandler db = new DatabaseHandler(this);
+
+        bSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, MainActivitySignUp.class));
+            }
+        });
+
     }
 }
