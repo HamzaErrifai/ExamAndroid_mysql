5c53008b15a63da568928e7db01e572f16a2b227
Hamza ERRIFAI <hamzaerrifai@hotmail.com>
2024-03-17 17:25:09 +0000

    fixed temperature Change

--- app/src/main/java/com/myapplicationsqlite/TemperatureActivity.java
+++ app/src/main/java/com/myapplicationsqlite/TemperatureActivity.java
@@ -3,15 +3,13 @@ package com.myapplicationsqlite;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.Toast;
-
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 
 public class TemperatureActivity extends AppCompatActivity {
 
@@ -30,81 +28,120 @@ public class TemperatureActivity extends AppCompatActivity {
         rgOutput = (RadioGroup) findViewById(R.id.rg_output);
         inputTemp = findViewById(R.id.inputTemp);
         outputTemp = findViewById(R.id.outputTemp);
-        //By default it is converting from celsius to celsius
+
         rbInput = findViewById(R.id.inputCelsius);
         rbOutput = findViewById(R.id.outputCelsius);
-
         bConvert = findViewById(R.id.bConvert);
 
+        //By default it is converting from celsius to Farenheit
+        verifyAndShow();
+        inputTemp.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String input = s.toString();
+                if (!input.isEmpty())
+                    verifyAndShow();
+
+            }
+        });
+        rgInput.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                verifyAndShow();
+            }
+        });
+        rgOutput.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                verifyAndShow();
+            }
+        });
 
         bConvert.setOnClickListener(new View.OnClickListener() {
-            String from = "Celsius";
-            String to = "Celsius";
+
 
             @Override
             public void onClick(View v) {
-                //get checked input
-                int checkedInputId = rgInput.getCheckedRadioButtonId();
-                if (checkedInputId == R.id.inputFarenheit) {
-                    from = "Farenheit";
-                } else if (checkedInputId == R.id.inputKelvin) {
-                    from = "Kelvin";
-                } else {
-                    from = "Celsius";
-                }
-                //get checked output
-                int checkedOutputId = rgOutput.getCheckedRadioButtonId();
-                if (checkedOutputId == R.id.outputFarenheit) {
-                    to = "Farenheit";
-                } else if (checkedOutputId == R.id.outputKelvin) {
-                    to = "Kelvin";
-                } else {
-                    to = "Celsius";
-                }
-                convertAndShow(from, to);
+                verifyAndShow();
             }
         });
 
     }
 
-    private double convertAndShow(String from, String to) {
-        double inputVal = Double.parseDouble(inputTemp.getText().toString());
-        double outputVal = 0;
+    private void verifyAndShow() {
+        String from = "Celsius";
+        String to = "Farenheit";
+        //get checked input
+        int checkedInputId = rgInput.getCheckedRadioButtonId();
+        if (checkedInputId == R.id.inputFarenheit) {
+            from = "Farenheit";
+        } else if (checkedInputId == R.id.inputKelvin) {
+            from = "Kelvin";
+        } else {
+            from = "Celsius";
+        }
+        //get checked output
+        int checkedOutputId = rgOutput.getCheckedRadioButtonId();
+        if (checkedOutputId == R.id.outputFarenheit) {
+            to = "Farenheit";
+        } else if (checkedOutputId == R.id.outputKelvin) {
+            to = "Kelvin";
+        } else {
+            to = "Celsius";
+        }
+
+        convertAndShow(from, to);
+    }
+
+    private void convertAndShow(String from, String to) {
+        double inputVal = 0;
+        if (!inputTemp.getText().toString().isEmpty())
+            inputVal = Double.parseDouble(inputTemp.getText().toString());
+        System.out.println("inputVal: " + inputVal);
         if (from != to) {
-            if(from =="Celsius"){
-                switch (to){
+            if (from == "Celsius") {
+                switch (to) {
                     case "Kelvin":
-                        outputTemp.setText(String.format("%,.2f",TemperatureConverter.celsiusToKelvin(inputVal)));
+                        System.out.println("INSIDE : From: " + from + " to: " + to);
+                        outputTemp.setText(String.format("%,.2f", TemperatureConverter.celsiusToKelvin(inputVal)));
                         break;
                     case "Farenheit":
-                        outputTemp.setText(String.format("%,.2f",TemperatureConverter.celsiusToFarenheit(inputVal)));
+                        outputTemp.setText(String.format("%,.2f", TemperatureConverter.celsiusToFarenheit(inputVal)));
                         break;
                 }
-            } else if (from =="Farenheit") {
-                switch (to){
+            } else if (from == "Farenheit") {
+                switch (to) {
                     case "Celsius":
-                        outputTemp.setText(String.format("%,.2f",TemperatureConverter.fahrenheitToCelsius(inputVal)));
+                        outputTemp.setText(String.format("%,.2f", TemperatureConverter.fahrenheitToCelsius(inputVal)));
                         break;
                     case "Kelvin":
-                        outputTemp.setText(String.format("%,.2f",TemperatureConverter.fahrenheitToKelvin(inputVal)));
+                        outputTemp.setText(String.format("%,.2f", TemperatureConverter.fahrenheitToKelvin(inputVal)));
                         break;
                 }
             } else if (from == "Kelvin") {
-                switch (to){
+                switch (to) {
                     case "Celsius":
-                        outputTemp.setText(String.format("%,.2f",TemperatureConverter.kelvinToCelsius(inputVal)));
+                        outputTemp.setText(String.format("%,.2f", TemperatureConverter.kelvinToCelsius(inputVal)));
                         break;
                     case "Farenheit":
-                        outputTemp.setText(String.format("%,.2f",TemperatureConverter.kelvinToFarenheit(inputVal)));
+                        outputTemp.setText(String.format("%,.2f", TemperatureConverter.kelvinToFarenheit(inputVal)));
                         break;
                 }
             }
-        }else {
+        } else {
             outputTemp.setText(Double.toString(inputVal));
 
         }
 
-
-        return 0;
     }
 }
