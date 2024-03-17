862b0843a06e1d31e4749834980996fa221a8e3a
Hamza ERRIFAI <hamzaerrifai@hotmail.com>
2024-03-17 03:07:36 +0000

    open and close Sidebar

--- app/src/main/java/com/myapplicationsqlite/MainActivity.java
+++ app/src/main/java/com/myapplicationsqlite/MainActivity.java
@@ -1,15 +1,24 @@
 package com.myapplicationsqlite;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawerLayout;
     EditText etLogin, etPassword;
     Button bLogin, bSignUp;
 
@@ -17,6 +26,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, myToolbar, R.string.open_nav, R.string.close_nav);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new CalculatriceFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_calculatrice);
+        }
+
 
         etLogin = findViewById(R.id.etLogin);
         etPassword = findViewById(R.id.etPassword);
@@ -40,5 +66,29 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.nav_calculatrice)
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new CalculatriceFragment()).commit();
+        else if (item.getItemId() == R.id.nav_temperature)
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new TemperatureFragment()).commit();
+        else if (item.getItemId() == R.id.nav_afficher)
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AffichageFragment()).commit();
+        else
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new CalculatriceFragment()).commit();
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
     }
 }
